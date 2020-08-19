2d4b5f912622862aee03cf6ea7b8b0a0ef1af786
Richard Kurwitz <kurwitz3@aol.com>
2020-08-19 02:24:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   puts"The line is currently empty."
  end
   while  array.length > 0
-  puts "The line is currently: #{array}."
+  puts "The line is currently: #{array.join}."
   array[] +=1 
 end
 end
