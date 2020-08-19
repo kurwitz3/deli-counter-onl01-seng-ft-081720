9b49ce705b42781f905bb0742cdc94517ac7ec5a
Richard Kurwitz <kurwitz3@aol.com>
2020-08-19 02:17:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,9 +13,10 @@ end
    puts "Welcome, #{name}. You are number #{array.length} in line."
  end
  def now_serving(array)
+    if array.length > 0 
     puts "Currently serving #{array[0]}."
     array.shift
-    if array.length == 0 
+  else
       puts "There is nobody waiting to be served!"
     end
     end
