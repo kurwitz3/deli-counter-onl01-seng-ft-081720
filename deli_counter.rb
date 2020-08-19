6bb8d1910206190df48f8205f794e55b18d4e287
Richard Kurwitz <kurwitz3@aol.com>
2020-08-19 01:29:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@ def line(array)
  x = 0
   if array.length == 0
   puts"The line is currently empty."
+ end
   while  array.length > 0
   puts "The line is currently #{x + 1}.#{array[0]}"
    +=1 
-end
 end
 end
