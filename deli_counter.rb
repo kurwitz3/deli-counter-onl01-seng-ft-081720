27feb020da7a08f7ac23a35ef9364e0f07b8dcb0
Richard Kurwitz <kurwitz3@aol.com>
2020-08-19 04:08:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,9 @@ def line(array)
   array.each_with_index do |index,name|
     new_array.push("#{name + 1}. #{index}")
   end
-  puts "The line is currently: #{new_array.join(" ")}"
-      if new_array.length == 0 
+ if new_array.length > 0
+ puts "The line is currently: #{new_array.join(" ")}"
+ else
   puts "The line is currently empty."
 end
  
