befb91c42f1fd9fd5c66633776d1b57896d2af85
Richard Kurwitz <kurwitz3@aol.com>
2020-08-19 03:03:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(array)
  
  array.each_with_index do |name,index|  
-   puts " #{name}. #{index}."
+   puts " #{index}. #{name}."
 end
   if array.length == 0 
   puts "The line is currently empty."
